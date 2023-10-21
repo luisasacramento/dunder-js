e3fab1204c98735cd0c0e4d24c9caf5b045d253b
Luisa Santos <luisacristina.sn@icloud.com>
2023-10-20 21:44:14 -0300

    att projeto em react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import react from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import GlobalStyles from './style/GlobalStyles'
+import Home from './pages/Home'
+import Episodios from './pages/Episodios'
+import Novidades from './pages/Novidades'
+import Personagens from './pages/Personagens'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <GlobalStyles />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/episodios" element={<Episodios />}/>
+          <Route path="/novidades" element={<Novidades />}/>
+          <Route path="/personagens" element={<Personagens />}/>
+        </Routes>
+      </Router>
   );
 }
 
 export default App;
+
